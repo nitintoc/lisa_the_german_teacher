757ea19af7a20c6daefe7149adffb283ab6fb5e5
Nitin Augustine <nitintoc@gmail.com>
2024-05-29 12:02:07 +0200

    changes authentication

--- AuthenticationCheck/authentication.py
+++ AuthenticationCheck/authentication.py
@@ -5,15 +5,14 @@ Function to check the validity of the openai api key
 import openai
 import os
 
-def authenticate(API_KEY: str) -> bool:
-    os.environ["OPENAI_API_KEY"] = API_KEY
-    openai.api_key = os.getenv("OPENAI_API_KEY")
-    
+def authenticate(API_KEY: str) -> bool:   
+    client = openai.OpenAI(api_key=API_KEY) 
     try:
-        # Making a test request to validate the API key
-        openai.Engine.list()  # This is a simple call to check API key validity
-        return True
-    except openai.error.AuthenticationError:
+
+        if client.models.list():
+        # Making a test request to validate the API key  # This is a simple call to check API key validity
+            return True
+    except openai.AuthenticationError:
         return False
     except Exception as e:
         print(f"An error occurred: {e}")
